c36198a04af0d3b8173926a14322dbd96c0a1eda
QMinh <leminhken124356@gmail.com>
2024-04-25 14:39:02 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Card from './assets/components/card'
+
+
+function App() {
+  return (
+    <div className='flex justify-center items-center'>
+      <Card />
+    </div>
+  )
+}
+
+export default App
---
+++ src/assets/components/card.jsx
@@ -0,0 +1,11 @@
+
+function Card() {
+    return (
+        <div className="border-s-emerald-700 bg-emerald-400">
+            <h1 className="text-white">Minh</h1>
+            <h2 className="text-white">Information Technology</h2>
+        </div>
+    );
+}
+
+export default Card
